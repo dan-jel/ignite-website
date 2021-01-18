49ccca3681ca0f4e4c1ea427f0ce6a9db300e58d
danjel <daniel.heese1@gmail.com>
2021-01-18 13:31:59 +0100

    home page restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ignite!</h1>
+      <Home />
     </div>
   );
 }
